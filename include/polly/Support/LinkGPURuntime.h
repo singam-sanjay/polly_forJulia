abb2504a97dca08806131fca424ed6fa856ab0af
Sanjay Sri Vallabh <sanjay.srivallabh@gmail.com>
2017-05-15 18:34:56 +0530

    Somewhat working version of LinkGPURuntime.h

--- include/polly/Support/LinkGPURuntime.h
+++ include/polly/Support/LinkGPURuntime.h
@@ -15,7 +15,10 @@
 #ifndef POLLY_LINK_GPURUNTIME
 #define POLLY_LINK_GPURUNTIME
 
-#include "GPURuntime/GPUJIT.h"
+extern "C"
+{
+  #include "GPURuntime/GPUJIT.c"
+}
 
 namespace polly {
   void ForceGPURuntimeLinking()
@@ -26,6 +29,16 @@ namespace polly {
     // delete it all as dead code, even with whole program optimization,
     // yet is effectively a NO-OP. As the compiler isn't smart enough
     // to know that getenv() never returns -1, this will do the job.
-    polly_getKernel(nullptr,nullptr);
+    polly_initContext();
+    polly_getKernel(nullptr, nullptr);
+    polly_freeKernel((PollyGPUFunction*)nullptr);
+    polly_copyFromHostToDevice(nullptr, (PollyGPUDevicePtr*)nullptr, 0);
+    polly_copyFromDeviceToHost((PollyGPUDevicePtr*)nullptr, nullptr, 0);
+    polly_synchronizeDevice();
+    polly_launchKernel((PollyGPUFunction*)nullptr, 0, 0, 0, 0, 0, (void**)nullptr);
+    polly_freeDeviceMemory((PollyGPUDevicePtr*)nullptr);
+    polly_freeContext((PollyGPUContext*)nullptr);
+    polly_synchronizeDevice();
   }
 }
+#endif
