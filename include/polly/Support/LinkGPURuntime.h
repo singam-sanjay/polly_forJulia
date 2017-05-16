1507e7c2468edcda64a7e3c341a7d0d76cd7a812
Sanjay Sri Vallabh <sanjay.srivallabh@gmail.com>
2017-05-16 19:03:41 +0530

    1. Stylistic changes 2. Used static struct instead of func call tp force link.

--- include/polly/Support/LinkGPURuntime.h
+++ include/polly/Support/LinkGPURuntime.h
@@ -1,4 +1,4 @@
-//===- ??FOLDER??/LinkGPURuntime.h -- Headerfile to help force-link GPURuntime --------------------===//
+//===- Support/LinkGPURuntime.h -- Headerfile to help force-link GPURuntime --------------------===//
 //////
 //////                     The LLVM Compiler Infrastructure
 //////
@@ -15,30 +15,31 @@
 #ifndef POLLY_LINK_GPURUNTIME
 #define POLLY_LINK_GPURUNTIME
 
-extern "C"
-{
-  #include "GPURuntime/GPUJIT.c"
+extern "C" {
+#include "GPURuntime/GPUJIT.c"
 }
 
 namespace polly {
-  void ForceGPURuntimeLinking()
-  {
-    if (std::getenv("bar") != (char*) -1)
-      return;
-    // We must reference GPURuntime in such a way that compilers will not
-    // delete it all as dead code, even with whole program optimization,
-    // yet is effectively a NO-OP. As the compiler isn't smart enough
-    // to know that getenv() never returns -1, this will do the job.
-    polly_initContext();
-    polly_getKernel(nullptr, nullptr);
-    polly_freeKernel((PollyGPUFunction*)nullptr);
-    polly_copyFromHostToDevice(nullptr, (PollyGPUDevicePtr*)nullptr, 0);
-    polly_copyFromDeviceToHost((PollyGPUDevicePtr*)nullptr, nullptr, 0);
-    polly_synchronizeDevice();
-    polly_launchKernel((PollyGPUFunction*)nullptr, 0, 0, 0, 0, 0, (void**)nullptr);
-    polly_freeDeviceMemory((PollyGPUDevicePtr*)nullptr);
-    polly_freeContext((PollyGPUContext*)nullptr);
-    polly_synchronizeDevice();
-  }
+  struct ForceGPURuntimeLinking {
+    ForceGPURuntimeLinking() {
+      if (std::getenv("bar") != (char*) -1)
+        return;
+      // We must reference GPURuntime in such a way that compilers will not
+      // delete it all as dead code, even with whole program optimization,
+      // yet is effectively a NO-OP. As the compiler isn't smart enough
+      // to know that getenv() never returns -1, this will do the job.
+      polly_initContextCL();
+      polly_initContextCUDA();
+      polly_getKernel(nullptr, nullptr);
+      polly_freeKernel(nullptr);
+      polly_copyFromHostToDevice(nullptr, nullptr, 0);
+      polly_copyFromDeviceToHost(nullptr, nullptr, 0);
+      polly_synchronizeDevice();
+      polly_launchKernel(nullptr, 0, 0, 0, 0, 0, nullptr);
+      polly_freeDeviceMemory(nullptr);
+      polly_freeContext(nullptr);
+      polly_synchronizeDevice();
+    }
+  } structure;
 }
 #endif
