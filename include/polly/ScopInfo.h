66a15788746a3f0d3135a3b3fce2ecbc8296789b
Sanjay Sri Vallabh <singapuram.sanjay@gmail.com>
2017-07-09 01:52:38 +0530

    Added doc strings.

--- include/polly/ScopInfo.h
+++ include/polly/ScopInfo.h
@@ -1613,10 +1613,10 @@ private:
   /// The name of the SCoP (identical to the regions name)
   std::string name;
 
-  ///
+  /// The ID to be assigned to the next Scop in a function
   static int NextScopID;
 
-  ///
+  /// The name of the function currently under consideration
   static std::string CurrentFunc;
 
   // Access functions of the SCoP.
@@ -1814,7 +1814,7 @@ private:
   /// A number that uniquely represents a Scop within its function
   const int ID;
 
-  ///
+  /// Return the ID for a new Scop within a function
   static int getNextID(std::string ParentFunc);
 
   /// Scop constructor; invoked from ScopBuilder::buildScop.
@@ -2387,7 +2387,9 @@ public:
   /// Check if the SCoP is to be skipped by ScopPass passes.
   bool isToBeSkipped() const { return SkipScop; }
 
+  /// Return the ID of the Scop
   int getID() const { return ID; }
+
   /// Get the name of the entry and exit blocks of this Scop.
   ///
   /// These along with the function name can uniquely identify a Scop.
